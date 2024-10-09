6746790e6b62deeeb1461909a5ecf5065804bc96
hassan faraji <iamhassanfaraji@gmail.com>
2024-10-09 11:30:15 +0330

    add test

---
+++ evdev_test.go
@@ -0,0 +1,35 @@
+package evdev
+
+import (
+  "testing" 
+)
+
+func TestGeneratorCapabilities(t *testing.T){
+  var expectation = map[string]int{
+    "EV_SYN": 4,
+	  "EV_KEY": 559,
+	  "EV_REL": 11,
+	  "EV_ABS": 42,
+	  "EV_MSC": 6,
+	  "EV_SW":  17,
+	  "EV_LED": 11,
+	  "EV_SND": 3,
+	  "EV_REP": 2,
+	  "EV_FF":  24,
+  }
+
+  capabilities := generatePossibleCapabilities() 
+  var unexpectedCapabilityCodesNumber = make(map[string]int) 
+
+  for _,v := range capabilities {
+    if expectation[v.CapabilityType.Name] == len(v.CapabilityCodes){
+      continue
+    }else{
+      unexpectedCapabilityCodesNumber[v.CapabilityType.Name] = len(v.CapabilityCodes)
+    }
+  } 
+    
+  if len(unexpectedCapabilityCodesNumber) != 0 { 
+    t.Error("there is'nt build capabilities in right range") 
+  } 
+}
